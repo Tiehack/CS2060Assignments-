f374918a96ca3261ab17850491773e69c3354360
Tiehack <nhallise@uccs.edu>
2022-08-29 14:56:55 -0600

    Add project files.

---
+++ CS2060 Assignment 1/this.c
@@ -0,0 +1,16 @@
+/*
+* The program randomly generates numbers
+* between 1 and 1000 for the user to guess.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+    printf("Is this correct?");
+    printf("\n");
+} 
+
+
